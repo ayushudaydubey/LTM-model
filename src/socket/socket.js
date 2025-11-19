3889c009832be8b4819220ea1b0bf28a9a662a07
ayushudaydubey <102001ayushdubey@gmail.com>
2025-11-20 03:56:24 +0530

    socket middelware

--- src/socket/socket.js
+++ src/socket/socket.js
@@ -1,8 +1,31 @@
 const {Server} = require("socket.io")
+const cookie = require('cookie')
+const jwt  = require("jsonwebtoken")
+const userModel = require("../models/userModel")
 
-  function initSocketServer(httpServer) {
+   function initSocketServer(httpServer) {
   
   const io  = new Server(httpServer,{})
+
+  io.use( async (socket,next)=>{
+    const cookies  = cookie.parse(socket.handshake.headers?.cookie || "")
+
+    if (!cookies.token) {
+      next(new Error ("unauthorized access - no token provided "))
+    }
+
+    try {
+      const decoded = jwt.verify(cookies.token,process.env.JWT_SEC_KEY)
+      const user  = await userModel.findById(decoded.id)
+
+      socket.user  = user
+      next()
+
+    } catch (error) {
+       next(new Error("authentication errro - invalid token "))
+    }
+    
+  })
 
   io.on("connection",(socket)=>{
     console.log("socket connected  ",socket.id);
