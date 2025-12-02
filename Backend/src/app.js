fc4ce8e4217d78d40683e32fd80cfb5dd1220f46
ayushudaydubey <102001ayushdubey@gmail.com>
2025-12-02 23:54:07 +0530

    bulid

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -5,11 +5,13 @@ const userRoutes = require("./routes/userRoute")
 const chatRoutes  = require("./routes/chatRoutes")
 const chatApiRoutes = require("./routes/chatApi")
 const cors  = require('cors')
+const path = require("path")
 
 
 // middelwares
 app.use(express.json())
 app.use(cookieParser())
+app.use(express.static(path.join(__dirname,"../public")))
 app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
@@ -24,6 +26,10 @@ app.use("/api/chat",chatRoutes)
 // additional API routes (newer version)
 app.use("/api/chatv2", chatApiRoutes)
 app.use("/api/chatv2", chatApiRoutes)
+
+app.get("*name",(req,res)=>{
+  res.sendFile(path.join(__dirname,"../public/index.html"))
+})
 
 
 
